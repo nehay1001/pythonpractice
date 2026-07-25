25aff807cfb724553fb4f717aae18dd83216fbf9
nehay1001 <nehayadav102006573@gmail.com>
2026-07-25 19:48:10 +0530

    first code 1

--- firstprogram.py
+++ firstprogram.py
@@ -1,5 +1,18 @@
-print("my name is neha")
-print("my age is 20")
-print("my name is neha : ", "my age is 20")
-print(35+35)
-print(35-35)
+# variable 
+name = "neha"
+age = 20
+born = 20.06
+print("My name is", name, "and I am", age, "years old. I was born in", born)
+print(name)
+print(age)
+print(born)
+
+print(type(name))
+print(type(age))
+print(type(born))
+
+age=23
+old=False
+a=None
+print(type(old))
+print(type(a))
