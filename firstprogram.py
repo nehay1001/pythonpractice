9943c47104ef054ffb5edd9459d9567d2059b9a4
nehay1001 <nehayadav102006573@gmail.com>
2026-07-28 00:12:18 +0530

    operators

--- firstprogram.py
+++ firstprogram.py
@@ -1,9 +1,25 @@
-#arithmatic operator
-a=5 
-b=2
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a%b)
-print(a**b)
+#relational operators
+a=50
+b=20
+print(a==b)  # False
+print(a!=b)  # True
+print(a>b)   # True
+print(a<b)   # False
+print(a>=b)  # True
+print(a<=b)  # False
+
+#assiment operators
+num=10
+num**=5
+print(num)  
+
+#logical operators 
+a=50
+b=30
+print(not False)
+print(not a>b)  # False
+
+val1=False
+val2=False
+print("and operator:", val1 and val2)
+print("or operator:", val1 or val2)
