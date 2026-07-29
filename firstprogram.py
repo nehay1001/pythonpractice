6f9362bfada6252fd472726a1b5a918a7b2f2b7c
nehay1001 <nehayadav102006573@gmail.com>
2026-07-29 14:33:49 +0530

    practice

--- firstprogram.py
+++ firstprogram.py
@@ -1,14 +1,25 @@
+#practice
+#write a program to input 2 numbers and print their sum
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+sum=num1+num2
+print("sum of",num1,"and",num2,"is",sum)
 
+#WAP to input side of a square and print its area
+side=int(input("enter side of square :"))
+area=side*side
+print("area of square is",area)
 
-name=input("enter your name:")
-age=input("enter your age:")
-marks=input("enter your marks:")
+#WAP to input 2 floating point numbers and print their average
+num1=float(input("enter first floating point number:"))
+num2=float(input("enter second floating point number:"))
+average=(num1+num2)/2
+# print("average =",average)
 
-print("welcome",name)
-print("enter age=",age)
-print("enter marks=",marks)
-
-age=int("enter your age:")
-marks=float("enter your marks:")
-print("your age is:",age)
-print("your marks is:",marks)
+#WAP to input 2 itegers number a and b print true if a is greater than b .if not print false
+a=int(input("enter first integer number:"))
+b=int(input("enter second integer number:"))
+if a>b:
+    print("true")
+else:
+    print("false")
