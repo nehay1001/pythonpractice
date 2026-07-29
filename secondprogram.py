dd311c793ae11ebc14732020fce5bf21948bf60d
nehay1001 <nehayadav102006573@gmail.com>
2026-07-29 15:04:40 +0530

    string escape

---
+++ secondprogram.py
@@ -0,0 +1,11 @@
+#string
+str1='this is a string'
+str2="this is a string"
+str3='''this is a string'''
+
+str1="this is string.we are creating in a python."
+print(str1)
+str1="this is string.\nwe are creating in a python."
+print(str1)
+str1="this is string.\twe are creating in a python."
+print(str1)
