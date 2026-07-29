ad9e89cc323d0599acd197f7dd29792d3640d209
nehay1001 <nehayadav102006573@gmail.com>
2026-07-29 15:44:54 +0530

    slicing

--- secondprogram.py
+++ secondprogram.py
@@ -1,14 +1,19 @@
-#indexing
+#slicing
 str="hello world"
-ch=str[0]
-print(ch)
-print(str[1])
-print(str[2])
-print(str[3])
-print(str[4])
-print(str[5])
-print(str[6])
-print(str[7])
-print(str[8])
-print(str[9])
-print(str[10])
+print(str[0:1])#h
+print(str[0:2])#he
+print(str[0:3])#hel
+print(str[0:4])#hell
+print(str[0:5])#hello
+print(str[0:6])#hello
+print(str[0:7])#hello w
+print(str[0:8])#hello wo
+print(str[0:9])#hello wor
+print(str[0:10])#hello worl
+print(str[0:11])#hello world
+print(str[0:12])#hello world
+print(str[1:5])#ello
+print(str[6:11])#world
+print(str[6:])#world
+print(str[:5])#hello
+print(str[5:len(str)])# world
