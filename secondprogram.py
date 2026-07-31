ba38a2ef2f052ef3917c18855a56f27064ebea41
nehay1001 <nehayadav102006573@gmail.com>
2026-07-31 14:18:01 +0530

    conditional statement

--- secondprogram.py
+++ secondprogram.py
@@ -1,40 +1,60 @@
-# slicing /negative indexing
-str="apple"
-print(str[-3:-1])
-print(str[-5:-1])
-print(str[-5:-2])
+age=21
+if(age >= 18):
+    print("i can drive and apply for license")
 
-#string functions
-str="i am studying python from apnacollege"
-print(str.endswith("ege"))  #return true if string end with substr
-print(str.endswith ("app"))
+age=24
+if(True):
+    print("can drive")
+    print("can vote")
 
+light="green"
+if(light=="red"):
+    print("stop")
+if(light=="green"):
+    print("go")
+if(light=="yellow"):
+    print("look")
 
-str="i am studying python from apnacollege"
-print(str.capitalize())# capitalized first later
-print(str)
-
-print(str.replace("o","a"))#replace all occurance old 
-print(str.replace("python","javascript"))
-
-print(str.find("o"))#return 1st index of 1st occurance
-str="i am from studing python from apnacollege"
-print(str.find("from"))
-print(str.find("q"))
-
-str="i am from studying python from apnacollege"
-print(str.count("from"))
-print(str.count("o"))
+print("end of code")
 
 
-#practice
-#WAP to input user'first name and print its length
-name=input("enter your name:")
-print("length of your name is",len(name))
+num=5
+if(num>2):
+    print("num greater then 2")
+if(num>3):
+    print("num greater then 3")
 
-#WAP to find the occurance of $ in a string
-str="i have 10$ and this $ i give to how now you count how many $ you have"
-print(str.count("$"))
+num=5
+if(num>2):
+    print("num greater then 2")
+elif(num>3):
+    print("num greater then 3")
+
+light="pink"
+if(light=="red"):
+    print("stop")
+elif (light=="green"):
+    print("go")
+elif(light=="yellow"):
+    print("look")
+else:
+    print("light is broken")
 
 
 
+age=14
+if(age>=18):
+    print("can vote")#indentation {}
+else:
+    print("cannot vote")
+
+
+marks=74
+if(marks>=90):
+    grade="A"
+elif(marks>=80 and marks<=90):
+    grade="B"
+elif(marks>=70 and marks<=80):
+    grade="C"
+else:
+    grade="D"
