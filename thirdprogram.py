e0b9b6550a251684242d9b4c0cfb0a5fb37a9291
nehay1001 <nehayadav102006573@gmail.com>
2026-07-31 17:06:04 +0530

    list 1

---
+++ thirdprogram.py
@@ -0,0 +1,19 @@
+#list in python
+marks1=89.98
+marks1=87.78
+marks1=45.89
+marks1=90.10
+marks1=75.06
+marks=[89.98, 87.78, 45.89, 90.10, 75.06]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0])
+print(marks[1])
+print(marks[2])
+print(marks[3])
+print(marks[4])
+
+student=("neha","92.60","20","mumbai")
+print(student)
+
