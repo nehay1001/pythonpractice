c59aa80e0abc1dc90e576941e87bc0ccf66e66b4
nehay1001 <nehayadav102006573@gmail.com>
2026-07-31 15:11:44 +0530

    practice3

--- secondprogram.py
+++ secondprogram.py
@@ -1,11 +1,32 @@
-#nesting
-age=95
-
-if(age>=18):
-    if(age>=80):
-        print("cannot drive")
-    else:
-        print("can drive")
+# practice
+#WAP to check if number entered by the user is odd or even.
+num=int(input("enter number:"))
+if(num%2==0):
+    print("EVEN")
 else:
-    print("cannot drive")
+    print("ODD")
+
+
+
+#WAP to find the greatest of 3 number entered by the user.
+a=int(input ("enter first number:"))
+b=int (input("enter second number :"))
+c=int(input("enter third number:"))
+
+if (a>=b and a>=c):
+    print("first number is largest ", a)
+elif(b>=c):
+    print ("second number is largest",b)
+else:
+    print("third number is largest", c)
+
+
+
+
+#WAP to check if number is a multiple of 7 or not.
+num=int(input("enter number:"))
+if(num%7==0):
+    print("multiple of 7")
+else:
+    print("not a multiple")
 
