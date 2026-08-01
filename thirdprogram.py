a6dfce2aa961d4380df6377e7ccb2afcdf369d0c
nehay1001 <nehayadav102006573@gmail.com>
2026-08-01 09:03:15 +0530

    list slicing

--- thirdprogram.py
+++ thirdprogram.py
@@ -1,19 +1,18 @@
-#list in python
-marks1=89.98
-marks1=87.78
-marks1=45.89
-marks1=90.10
-marks1=75.06
-marks=[89.98, 87.78, 45.89, 90.10, 75.06]
-print(marks)
-print(type(marks))
-print(len(marks))
-print(marks[0])
-print(marks[1])
-print(marks[2])
-print(marks[3])
-print(marks[4])
 
-student=("neha","92.60","20","mumbai")
+student=["radhika",45.67,12,"mumbai"]# this onlu in list not in string
+print(student[0])
+student[0]="priyanshi"
 print(student)
 
+student==["radhika",45.67,12,"mumbai"]
+print(student[3])
+
+
+#list slicing
+marks=[45,67,89,90,78,56]
+print(marks[1:5])#1 to 4 index value
+print(marks[1:])#1 to last index value
+print(marks[:5])#0 to 4 index value
+print(marks[:])#0 to last index value
+print(marks[-5:-1])#-5 to -2 index value
+print(marks[-5:-3])#-5 to -4 index value
