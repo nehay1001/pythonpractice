93974bc77363fca9785129ba6849cef173f49daa
nehay1001 <nehayadav102006573@gmail.com>
2026-08-02 19:00:11 +0530

    python set

--- fourthprogram.py
+++ fourthprogram.py
@@ -1,131 +1,61 @@
-# dictionary in python
-inf={
-    "key":"value",
-    "name":"apnacollege",
-    "learning":"python",
-    "age":20,
-    "is_student":True,
-    "marks":92.60,
-}
+#set in python 
+collection={1,2,3,4}
+print(collection)
+print(type(collection))
 
-print(inf)
+collection={1,2,2,2,"hello","world","world"}
+print(collection)
 
+collection={1,2,2,2,"hello","world","world",4}
+print(collection)
+print(len(collection))
 
-inf={
-    
-    "name":"apnacollege",
-    "subject":["c","python","java"],
-    "tuple":("dictionary","list","tuple"),
-    "learning":"python",
-    "age":20,
-    "is_student":True,
-    "marks":92.60,
-}
+collection={}  #but this is empty dictinory
+print(type(collection))
 
+collection=set() #empty set: syntax
+print(type(collection))
 
+#set methode
+collection=set()
+collection.add(1)
+collection.add(2)
+collection.add(2)
+collection.add(3)
+collection.add("nehayadav")
+collection.add((1,2,3,))
+print(collection)
+print(len(collection))
 
-print(inf)
-print(type(inf))
+collection.remove(1)
+collection.remove(2)
+print(collection)
 
-inf={
-    
-    "name":"apnacollege",
-    "subject":["c","python","java"],
-    "tuple":("dictionary","list","tuple"),
-    "learning":"python",
-    "age":20,
-    "is_student":True,
-    "marks":92.60,
-}
+collection=set()
+collection.add(1)
+collection.add(2)
+collection.add(2)
+collection.add(3)
+collection.add("nehayadav")
+collection.add((1,2,3,))
 
-print(inf["name"])
-print(inf["subject"])
-print(inf["tuple"])
-print(inf["learning"])
-print(inf["age"])
-print(inf["is_student"])
-print(inf["marks"])
+collection.clear()
+print(collection)
+print(len(collection))
 
-inf["name"]="nehayadav"#ovewrite or we can put number also
-print(inf)
+collection={"hello","apnacollege","world","coding","python"}
+print(collection.pop())
+print(collection.pop())
+print(collection.pop())
 
-inf["name"]="neha"
-inf["surname"]="yadav"
-print(inf)
+set1={1,2,3,}
+set2={2,3,4,}
+print(set1.union(set2)) #{1,2,3,4}
+print(set1)
+print(set2)
 
-nul_dict={}
-print(nul_dict)
-
-nul_dict={}
-nul_dict["name"]="neha"
-print(nul_dict)
-
-
-#nested dictionary
-student={
-        "name":"neha",
-        "subject":{
-            "physics":90,
-            "maths":80,
-            "chemistry":70,
-        }
-
-}
-
-print(student)
-print(student["subject"])
-print(student["subject"]["physics"])
-
-
-#methods in dictionary
-inf={
-    "name":"apnacollege",
-    "subject":["c","python","java"],
-    "tuple":("dictionary","list","tuple"),
-    "learning":"python",
-    "age":20,
-    "is_student":True,
-    "marks":92.60,
-}
-print(inf.keys())# inf.key()
-print(list(inf.keys()))
-print(len(list(inf.keys())))
-print(len(inf))
-
-print(inf.values())# inf.values()
-print(list(inf.values()))
-print(len(list(inf.values())))
-print(len(inf))
-
-
-print(inf.items())# inf.items()
-print(list(inf.items()))
-pairs=list(inf.items())
-print(pairs[0])
-print(pairs[1])
-
-print(inf.get("name"))# inf.get("name")
-print(inf["name"])# inf["name"]
-
-print(inf.get("name2"))# not error-> none
-print(inf["name2"])#error
-
-print("BEFORE")
-print(inf["name2"])#error
-print("AFTER")
-
-student.update({"city":"delhi"})
-print(student)
-
-new_dict={"city":"delhi",}
-student.update(new_dict)
-print(student)
-
-
-new_dict={"city":"delhi","age":"20",}
-student.update(new_dict)
-print(student)
-
-new_dict={"name":"yadav",}
-student.update(new_dict)
-print(student)
+set1={1,2,3,}
+set2={2,3,4,}
+print(set1.intersection(set2)) #{2,3}
+print(set1)
+print(set2)
