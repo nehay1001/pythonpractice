e73b9301b455fa8be7ebd64f1ba0d7a05afdb8a2
nehay1001 <nehayadav102006573@gmail.com>
2026-08-05 17:18:50 +0530

    practice using for and range

--- fivethprogram.py
+++ fivethprogram.py
@@ -1,37 +1,13 @@
-# rang
-print(range(5))
-
-seq=range(5)
-print(seq[0])
-print(seq[1])
-print(seq[2])
-print(seq[3])
-print(seq[4])
-
-seq=range(5)
-for i in seq:
+# practice using for and range
+# print numbers from 1 to 100.
+for i in range(1,101):
     print(i)
 
-seq=range(10)
-for i in seq: 
- print(i)
-
- for i in range(10):# range(stop)
+#print numbers from 100 to 1.
+for i in range (100,0,-1):
     print(i)
 
-
-for i in range(2,10):# range(star,stop)
-    print(i)
-
-for i in range(2,10,2):# range(star,stop,step)
- print(i)
-
-for i in range(2,100,2):
-    print(i)
-
-for i in range(2,101,2):
-   print(i)
-
-for i in range(1,100,2):
-    print(i)
-
+#print multiplication table of a number n.
+n=int(input("enter number:"))
+for i in range(1,11):
+    print(n*i)
