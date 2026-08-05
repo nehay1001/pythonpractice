96bc3a5a9739046021cdb04f40556b80322dbb9a
nehay1001 <nehayadav102006573@gmail.com>
2026-08-05 17:39:25 +0530

    practice6

--- fivethprogram.py
+++ fivethprogram.py
@@ -1,13 +1,52 @@
-# practice using for and range
-# print numbers from 1 to 100.
-for i in range(1,101):
-    print(i)
+# pass staement in loops
+for i in range(5):
+    pass
+print("some useful work")
 
-#print numbers from 100 to 1.
-for i in range (100,0,-1):
-    print(i)
+for i in range(5):
+    pass
 
-#print multiplication table of a number n.
-n=int(input("enter number:"))
-for i in range(1,11):
-    print(n*i)
+if i>5:
+    pass
+print("some useful work")
+
+# practice 
+# WAP to find the sum of first n numbers.(using while)
+n=5
+
+sum=0
+for i in range(1,n+1):
+    print(i)
+    sum+=i
+print("total sum=",sum)  #or
+
+n=7
+sum=0
+i=1
+while i<=n:
+    sum+=i
+    i+=1
+
+print("total sum=",sum) 
+
+
+
+#WAP to find the factorial of first n numbers.(using for)
+n=3
+fact=1
+i=1
+while i<=n:
+    fact*=i
+    i+=1
+
+print("factorial=",fact)
+
+
+n=5
+fact=1
+for i in range (1,n+1):
+
+    fact*=i
+    
+
+print("factorial=",fact)
