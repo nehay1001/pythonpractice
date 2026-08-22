b449df4fdd391c6ef1a5d51ccd3fbc27a2e9c098
nehay1001 <nehayadav102006573@gmail.com>
2026-08-22 19:26:45 +0530

    practice OOps

--- ninthprogram.py
+++ ninthprogram.py
@@ -1,106 +1,79 @@
-# pollymorphism
+# let's practice
+ 
+# Qs1. define a circle class to creat circle with radius r using the constructor.
+# define an area () method of class which calculatethe area of the circle.
+# define a perimeter () method of the class which allows you to calculate the perimeter of the circle
 
-#print( 1 + 2 ) #3
-#print(type(1))
+class Circle:
+    def __init__(self,radius):
+        self.radius=radius
 
-#print("apna"+"college") #concatenate
-#print(type("apna"))
+    def area(self):
+        return (22/7) * self.radius **2
 
-#print([1,2,3]+[4,5,6]) #merge
-#print(type ([1,2,3]) )
+    def perimeter(self):
+        return 2 * (22/7) *self.radius
 
-class Complex:
-    def __init__(self,real,img):
-        self.real=real
-        self.img=img
-
-    def showNumber(self):
-        print(self.real,"i+",self.img,"j")
-
-num1=Complex(1,3)
-num1.showNumber()
-
-num2=Complex(4,6)
-num2.showNumber()
+c1=Circle(21)
+print(c1.area())
+print(c1.perimeter())
 
 
 
+# Qs2. define a Employee class with attributes role .department and salary.this class also have showDetails() method.
+     # creat an engineer class that inherits properties from employee and has additional attributes : name and age
 
-class Complex:
-    def __init__(self,real,img):
-        self.real=real
-        self.img=img
+class Employee:
+    def __init__(self,role,dept,salary):
+        self.role=role
+        self.dept=dept
+        self.salary=salary
 
-    def showNumber(self):
-        print(self.real,"i+",self.img,"j")
+    def showDetails(self):
+        print("role=",self.role)
+        print("dept=",self.dept)
+        print("salary=",self.salary)
 
-    def add(self,num2):
-        newReal=self.real + num2.real
-        newImg=self.img + num2.img
-        return Complex(newReal,newImg)
-
-num1=Complex(1,3)
-num1.showNumber()
-
-num2=Complex(4,6)
-num2.showNumber()
-
-num3=num1 .add(num2)
-num3.showNumber()
+e1=Employee("accountant","Finance","60,000")
+e1.showDetails()
 
 
 
+class Employee:
+    def __init__(self,role,dept,salary):
+        self.role=role
+        self.dept=dept
+        self.salary=salary
 
+    def showDetails(self):
+        print("role=",self.role)
+        print("dept=",self.dept)
+        print("salary=",self.salary)
 
-class Complex:
-    def __init__(self,real,img):
-        self.real=real
-        self.img=img
+class Engineer(Employee):
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        super().__init__("Engineer","IT","75,000")
 
-    def showNumber(self):
-        print(self.real,"i+",self.img,"j")
-
-    def __add__(self,num2):
-        newReal=self.real + num2.real
-        newImg=self.img + num2.img
-        return Complex(newReal,newImg)
-
-num1=Complex(1,3)
-num1.showNumber()
-
-num2=Complex(4,6)
-num2.showNumber()
-
-num3=num1 + num2
-num3.showNumber()
+engg1=Engineer("Elon Musk",40)
+engg1.showDetails()
 
 
 
 
+# creat class called order which store item and its price.    use Dunder function _ _ gt _ _() to convey that:
+# order1>order2 if price of order1 >price of order 2
 
-class Complex:
-    def __init__(self,real,img):
-        self.real=real
-        self.img=img
+class Order:
+    def __init__(self,item, price):
+        self.item=item
+        self.price=price
 
-    def showNumber(self):
-        print(self.real,"i+",self.img,"j")
+    def __gt__(self,odr2):
+        return self.price > odr2.price
 
-    def __add__(self,num2):
-        newReal=self.real + num2.real
-        newImg=self.img + num2.img
-        return Complex(newReal,newImg)
+odr1=Order("chips",20)
+odr2=Order("tea",15)
 
-    def __sub__(self,num2):
-            newReal=self.real - num2.real
-            newImg=self.img - num2.img
-            return Complex(newReal,newImg)
-
-num1=Complex(1,3)
-num1.showNumber()
-
-num2=Complex(4,6)
-num2.showNumber()
-
-num3=num1 - num2
-num3.showNumber()
+print(odr1 > odr2)   # true
