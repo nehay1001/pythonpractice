a4f405b0afb23bd2c1a13bc61bbbbc8011748990
nehay1001 <nehayadav102006573@gmail.com>
2026-08-22 16:55:24 +0530

    classmethod

--- ninthprogram.py
+++ ninthprogram.py
@@ -1,136 +1,55 @@
-# inheritance
-class Car:
+# class method
+class Person:
+    name="anonymous"
 
-    @staticmethod
-    def start():
-        print("car started..")
-
-    @staticmethod
-    def stop():
-        print("car stoped..")
-
-class ToyotaCar(Car):
-    def __init__(self,name):
+    def changeName(self,name):
         self.name=name
 
-car1=ToyotaCar("fortuner")
-car2=ToyotaCar("prius")
-
-print(car1.name)
-
-print(car1.start())              
+p1=Person()
+p1.changeName("neha yadav")
+print(p1.name)
+print(Person.name)
 
 
-class Car:
 
-    color="black"
+class Person:
+    name="anonymous"
 
-    @staticmethod
-    def start():
-        print("car started..")
+    def changeName(self,name):
+        Person.name=name
 
-    @staticmethod
-    def stop():
-        print("car stoped..")
-
-class ToyotaCar(Car):
-    def __init__(self,name):
-        self.name=name
-
-car1=ToyotaCar("fortuner")
-car2=ToyotaCar("prius")
-
-print(car1.color)
+p1=Person()
+p1.changeName("neha yadav")
+print(p1.name)
+print(Person.name)
 
 
 
 
-class Car:
+class Person:
+    name="anonymous"
 
-    @staticmethod
-    def start():
-        print("car started..")
+    def changeName(self,name):
+        self.__class__.name="neha"
 
-    @staticmethod
-    def stop():
-        print("car stoped..")
-
-class ToyotaCar(Car):
-    def __init__(self,brand):
-        self.brand=brand
-
-class Fortuner (ToyotaCar):
-    def __init__(self,type):
-        self.type=type
-
-car1=Fortuner("diesel")
-car1.start()
+p1=Person()
+p1.changeName("neha yadav")
+print(p1.name)
+print(Person.name)
 
 
 
-class A:
-    varA="welcome to class A"
+class Person:
+    name="anonymous"
 
-class B:
-    varB="welcome to class B"
+    #def changeName(self,name):
+        #self.__class__.name="neha"
 
-class C(A,B):
-    varC="welcome to class C"
+    @ classmethod
+    def changeName(cls,name):
+        cls.name=name
 
-c1=C()
-
-print(c1.varC)
-print(c1.varB)
-print(c1.varA)
-
-
-
-class Car:
-
-    def __init__(self,type):
-            self.type=type
-
-    @staticmethod
-    def start():
-        print("car started..")
-
-    @staticmethod
-    def stop():
-        print("car stoped..")
-
-class ToyotaCar(Car):
-    def __init__(self,name,type):
-        self.name=name
-        super(). __init__(type)
-
-car1=ToyotaCar("prius","electric")
-print(car1.type)
-
-
-
-
-class Car:
-
-    def __init__(self,type):
-            self.type=type
-
-    @staticmethod
-    def start():
-        print("car started..")
-
-    @staticmethod
-    def stop():
-        print("car stoped..")
-
-class ToyotaCar(Car):
-    def __init__(self,name,type):
-        super(). __init__(type)
-        self.name=name
-        super().start()
-
-car1=ToyotaCar("prius","electric")
-print(car1.type)
-
-
-
-
+p1=Person()
+p1.changeName("neha yadav")
+print(p1.name)
+print(Person.name)
