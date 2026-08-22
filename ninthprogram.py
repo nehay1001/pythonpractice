73ae70fcc831529b7b4cb48fda057d727591be97
nehay1001 <nehayadav102006573@gmail.com>
2026-08-22 15:41:19 +0530

    private attributes and method

--- ninthprogram.py
+++ ninthprogram.py
@@ -1,18 +1,59 @@
-# del key
-class Student:
-    def __init__(self,name):
-         self.name=name
+class Account:                               #for public
+    def __init__ (self,acc_no,acc_pass):   
+        self.acc_no=acc_no
+        self.acc_pass=acc_pass
 
-s1=Student("neha")
-print(s1)
-del s1
-# print (s1)  #if we print then error show becoz we use del s1
+acc1=Account("12345","abcde")
+print(acc1.acc_no)
+print(acc1.acc_pass)
 
-class Student:
-    def __init__(self,name):
-         self.name=name
+#class Account:                               #for private 
+    #def __init__ (self,acc_no,acc_pass):
+        #self.acc_no=acc_no
+        #self.__acc_pass=acc_pass
 
-s1=Student("neha")
-print(s1.name)
-del s1.name
-# print (s1.name)  # if we print then error show becoz we use del s1.name
+#acc1=Account("12345","abcde")
+#print(acc1.acc_no)
+#print(acc1.__acc_pass)                       # give error
+
+class Account:                               #for private 
+    def __init__ (self,acc_no,acc_pass):
+        self.acc_no=acc_no
+        self.__acc_pass=acc_pass
+
+    def reset_pass(self):
+        print(acc1.__acc_pass)
+
+acc1=Account("12345","abcde")
+
+print(acc1.acc_no)
+print(acc1.reset_pass())
+
+
+#class Person:                       # its private (__) that's why give error
+   # __name="anonymous"
+#p1=Person()
+#print(p1.__name)
+
+
+#class Person:                       # its private (__)that's why give error
+    #__name="anonymous"
+
+    #def __hello():
+       # print("hello person!")
+
+#p1=Person()
+#print(p1.__hello())
+
+class Person:                       
+    __name="anonymous"
+
+    def __hello(self):
+       print("hello person!")
+
+    def welcome(self):
+        self.__hello()
+
+p1=Person()
+print(p1.welcome())
+
